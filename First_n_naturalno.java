d7cae0dd7065a1e4713f5d227beee142326e2d57
abdulsaber <abdulsaber.as@gmail.com>
2022-01-24 19:48:52 +0530

    initial commit

---
+++ First_n_naturalno.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class First_n_naturalno{
+    public static void main(String[] args) {
+        int n,i=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n");
+        n=sc.nextInt();
+        //using while loop
+        while(i<=n){
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+}
